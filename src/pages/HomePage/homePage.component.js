5f91cdc85ea9f5b921221e3ba65a5c2c813bd70a
MinaSamirSaad <minasamir.nashid@gmail.com>
2021-09-27 23:17:53 +0200

    make home page component and add inside it directory, menu item component and styling them

---
+++ src/pages/HomePage/homePage.component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./homepage.styles.scss"
+import Directory from '../../components/directory/directory.component'
+export const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
